46a115dbfa6b1f4e92f4dfd11d3b79baace8b14b
shauryathedoglover <155382370+shauryathedoglover@users.noreply.github.com>
2024-08-18 14:49:38 +0530

    BDAY.py

---
+++ BDAY.py
@@ -0,0 +1,8 @@
+def func1():
+name = input("enter your name")
+BDate = int(input("enter your birthDay"))
+today = 18,08-2024
+if BDate == today:
+   print("HAPPY BIRTHDAY",name)
+else:
+   print("fuck of")
